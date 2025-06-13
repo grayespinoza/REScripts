56d062a012aa8de1593fe4cc4dd991c9ee2c2f97
Grey <grey@grayespinoza.dev>
2025-06-12 19:31:07 -0700

    feat: extend cursor script beyond RE4

--- disable_cursor.lua
+++ disable_cursor.lua
@@ -3,6 +3,18 @@ re.on_pre_gui_draw_element(function(element, context)
   if game_object == nil then return true end
 
   local name = game_object:call("get_Name")
+
+  -- Cursor for RE7
+  if name == "MouseCursorGUI" then
+    return false
+  end
+
+  -- Cursor for RE2 and RE3
+  if name == "GUI_MouseCursor" then
+    return false
+  end
+
+  -- Cursor for RE4
   if name == "Gui_ui0300" then
     return false
   end
