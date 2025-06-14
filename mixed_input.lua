84b56e8ec93a898070b383d2177e3b14fba39a47
Grey <grey@grayespinoza.dev>
2025-06-13 18:07:01 -0700

    fix: learn what namespaces actually are

--- mixed_input.lua
+++ mixed_input.lua
@@ -1,33 +1,17 @@
 local game_name = reframework:get_game_name()
 
 local function postUpdate()
-  -- Input Device Manager for RE2
-  if game_name == "re2" then
-    input_system = sdk.get_managed_singleton("app.ropeway.InputSystem")
-  end
-
-  -- Input Device Manager for RE3
-  if game_name == "re3" then
-    input_system = sdk.get_managed_singleton("offline.InputSystem")
-  end
-
+  local input_system = sdk.get_managed_singleton(sdk.game_namespace("InputSystem"))
   local inputMode = input_system:call("get_InputMode")
+
   if inputMode == 1 then
     inputMode = 0
   end
 end
 
-if game_name == "re2" then
+if game_name == "re2" or game_name == "re3" then
   sdk.hook(
-    sdk.find_type_definition("app.ropeway.InputSystem"):get_method("update"),
-    nil,
-    postUpdate
-  )
-end
-
-if game_name == "re3" then
-  sdk.hook(
-    sdk.find_type_definition("offline.InputSystem"):get_method("update"),
+    sdk.find_type_definition(sdk.game_namespace("InputSystem")):get_method("update"),
     nil,
     postUpdate
   )
