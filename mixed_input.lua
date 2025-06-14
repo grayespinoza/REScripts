73683e2655a0a7f183abec185e1c5c69a75fa21c
Grey <grey@grayespinoza.dev>
2025-06-14 16:51:14 -0700

    fix: remove use of game_name in mixed input script

--- mixed_input.lua
+++ mixed_input.lua
@@ -8,19 +8,17 @@ if not is_re7 and not is_re2 and not is_re3 and not is_re8 then
 end
 log.info("[mixed_input.lua] loaded")
 
-local game_name = reframework:get_game_name()
-
 local function postHIDManager()
   local hid_manager = sdk.get_managed_singleton(sdk.game_namespace("HIDManager"))
 
-  if game_name == "re7" then
+  if is_re7 then
     if hid_manager:get_field("<inputMode>k__BackingField") == 0 then
       hid_manager:set_field("<inputMode>k__BackingField", 1)
     end
   end
 
   -- This if-block courtesy of @juliohintze and @WiFiAdapter on GitHub
-  if game_name == "re8" then
+  if is_re8 then
     local inputMode = hid_manager:call("get_inputMode")
     if inputMode.V == 1 then
       inputMode.V = 0
@@ -37,7 +35,7 @@ local function postInputSystem()
   end
 end
 
-if game_name == "re7" or game_name == "re8" then
+if is_re7 or is_re8 then
   sdk.hook(
     sdk.find_type_definition(sdk.game_namespace("HIDManager")):get_method("doUpdate"),
     nil,
@@ -45,7 +43,7 @@ if game_name == "re7" or game_name == "re8" then
   )
 end
 
-if game_name == "re2" or game_name == "re3" then
+if is_re2 or is_re3 then
   sdk.hook(
     sdk.find_type_definition(sdk.game_namespace("InputSystem")):get_method("update"),
     nil,
