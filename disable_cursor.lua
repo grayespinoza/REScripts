933b90f66079d8d577e9c59f21f5d78e304a09ea
Grey <grey@grayespinoza.dev>
2025-06-14 16:43:34 -0700

    fix: add game checks in scripts
    Closes #4:

--- disable_cursor.lua
+++ disable_cursor.lua
@@ -1,3 +1,14 @@
+local is_re7 = reframework:get_game_name() == "re7"
+local is_re2 = reframework:get_game_name() == "re2"
+local is_re3 = reframework:get_game_name() == "re3"
+local is_re8 = reframework:get_game_name() == "re8"
+local is_re4 = reframework:get_game_name() == "re4"
+
+if not is_re7 and not is_re2 and not is_re3 and not is_re8 and not is_re4 then
+    return
+end
+log.info("[disable_cursor.lua] loaded")
+
 re.on_pre_gui_draw_element(function(element, context)
   local game_object = element:call("get_GameObject")
   if game_object == nil then return true end
@@ -5,23 +16,31 @@ re.on_pre_gui_draw_element(function(element, context)
   local name = game_object:call("get_Name")
 
   -- Cursor for RE7
-  if name == "MouseCursorGUI" then
-    return false
+  if is_re7 then
+    if name == "MouseCursorGUI" then
+      return false
+    end
   end
 
   -- Cursor for RE2 and RE3
-  if name == "GUI_MouseCursor" then
-    return false
+  if is_re2 or is_re3 then
+    if name == "GUI_MouseCursor" then
+      return false
+    end
   end
-  
+
   -- Cursor for RE8
-  if name == "GUIMouseCursor" then
-    return false
+  if is_re8 then
+    if name == "GUIMouseCursor" then
+      return false
+    end
   end
 
   -- Cursor for RE4
-  if name == "Gui_ui0300" then
-    return false
+  if is_re4 then
+    if name == "Gui_ui0300" then
+      return false
+    end
   end
 
   return true
--- disable_letterbox.lua
+++ disable_letterbox.lua
@@ -1,3 +1,10 @@
+local is_re4 = reframework:get_game_name() == "re4"
+
+if not is_re4 then
+    return
+end
+log.info("[disable_letterbox.lua] loaded")
+
 re.on_pre_gui_draw_element(function(element, context)
   local game_object = element:call("get_GameObject")
   if game_object == nil then return true end
@@ -5,8 +12,10 @@ re.on_pre_gui_draw_element(function(element, context)
   local name = game_object:call("get_Name")
 
   -- Cutscene Letterbox in RE4
-  if name == "Gui_ui2510" then
-    return false
+  if is_re4 then
+    if name == "Gui_ui2510" then
+      return false
+    end
   end
 
   return true
