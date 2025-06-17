588b90bdd8b69076e3fac996480e2df3b558368b
Grey <grey@grayespinoza.dev>
2025-06-17 11:23:21 -0700

    fix: remove right analog functionality in favor of flick stick

--- mixed_input.lua
+++ mixed_input.lua
@@ -12,23 +12,12 @@ local function postHIDManager()
   local hid_manager = sdk.get_managed_singleton(sdk.game_namespace("HIDManager"))
 
   if is_re7 then
-    local pad_manager = sdk.get_managed_singleton(sdk.game_namespace("PadManager"))
-    local activePad = pad_manager:call("get_activePad")
-    if activePad:call("getStickRightVertical") ~= 0 or activePad:call("getStickRightHorizontal") ~= 0 then
-      return
-    end
     if hid_manager:get_field("<inputMode>k__BackingField") == 0 then
       hid_manager:set_field("<inputMode>k__BackingField", 1)
     end
   end
 
   if is_re8 then
-    local pad_manager = sdk.get_managed_singleton(sdk.game_namespace("HIDPadManager"))
-    local activePad = pad_manager:call("get_activePad")
-    if activePad:call("getStickRightVertical") ~= 0 or activePad:call("getStickRightHorizontal") ~= 0 then
-      hid_manager:get_field("<inputMode>k__BackingField"):set_field("V", 0)
-      return
-    end
     if hid_manager:get_field("<inputMode>k__BackingField"):get_field("V") == 0 then
       hid_manager:get_field("<inputMode>k__BackingField"):set_field("V", 1)
     end
@@ -37,11 +26,6 @@ end
 
 local function postInputSystem()
   local input_system = sdk.get_managed_singleton(sdk.game_namespace("InputSystem"))
-  local inputMode = input_system:call("get_InputMode")
-
-  if inputMode == 1 then
-    inputMode = 0
-  end
 end
 
 if is_re7 or is_re8 then
