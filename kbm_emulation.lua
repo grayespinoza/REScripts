f7f24b23ff8cb72b06016f491d2328703c3c87f9
Grey <grey@grayespinoza.dev>
2025-08-04 19:55:43 -0700

    chore: remove RE2 and RE3 kbm emulation placeholders
    Closes #2:

--- kbm_emulation.lua
+++ kbm_emulation.lua
@@ -1,9 +1,7 @@
 local is_re7 = reframework:get_game_name() == "re7"
-local is_re2 = reframework:get_game_name() == "re2"
-local is_re3 = reframework:get_game_name() == "re3"
 local is_re8 = reframework:get_game_name() == "re8"
 
-if not is_re7 and not is_re2 and not is_re3 and not is_re8 then
+if not is_re7 and not is_re8 then
   return
 end
 log.info("[kbm_emulation.lua] loaded")
@@ -24,14 +22,6 @@ local function postHIDManager()
   end
 end
 
-local function postInputSystem()
-  local input_system = sdk.get_managed_singleton(sdk.game_namespace("InputSystem"))
-end
-
 if is_re7 or is_re8 then
   sdk.hook(sdk.find_type_definition(sdk.game_namespace("HIDManager")):get_method("doUpdate"), nil, postHIDManager)
-end
-
-if is_re2 or is_re3 then
-  sdk.hook(sdk.find_type_definition(sdk.game_namespace("InputSystem")):get_method("update"), nil, postInputSystem)
 end
